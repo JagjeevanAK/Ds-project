2f948f6136b8bb72f741face10ebebd8777047e4
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2024-09-06 07:40:56 +0530

    init

--- src/components/Representation.jsx
+++ src/components/Representation.jsx
@@ -144,8 +144,191 @@ export default function Representation(){
                 <div className="flex justify-center text-2xl font-bold my-8">Code(Adjacency List)</div>
             </div>
             <div>
-                
+                <span className="font-semibold">Adjacency Matrix:</span>
+                <span>
+                    The adjacency matrix
+                    structure augments the edge list structure with
+                    a matrix where each row and column
+                    corresponds to a vertex. It is a two
+                    dimensional matrix form where the rows
+                    represent source vertices and columns
+                    represent destination vertices. Data on edges
+                    and vertices is stored externally. Between
+                    each pair of vertices, the cost of one edge is
+                    to be stored. This shows which vertices are
+                    adjacent to one another. We know that two
+                    vertices are said to be adjacent if there is an
+                    edge connecting them. For a graph of n
+                    vertices, the dimensions of adjacency matrix
+                    will be n*n.
+                </span>
+                <p>
+                    In the case of directed graphs, suppose if
+                    vertex j is adjacent to vertex i then there will
+                    be an edge from i to j and vice versa. For a
+                    given vertex i, its adjacent vertices will be
+                    determined by looking at the row entry (i,
+                    [1…n]) of the matrix. If the value is true then it
+                    indicates that there exists an edge from i to j
+                    and false indicates no edge exists.
+                </p>
+                <p>
+                    In case of an undirected graph, the matrix
+                    values will be populated with Boolean values.
+                    The values of (i, j) and (j, i) are equal which
+                    means the adjacency matrix for an undirected
+                    graph is always symmetric along the diagonal.
+                </p>
+                <p>
+                    In a weighted graph, the Boolean values will
+                    be the costs of the edges connecting two
+                    vertices of a graph. Generally an adjacency
+                    matrix is used to represent weighted graphs. If
+                    adj[i][j] = w , then we will say that there is an
+                    edge from vertex i to vertex j with weight w
+                    (can be any positive number). There are some
+                    cases where zero can also be the possible
+                    weight of the edge, then we have to store
+                    some sentinel value for non-existent edge,
+                    which can be a negative value; since the
+                    weight of the edge is always a positive
+                    number.
+                </p>
+                <p>
+                    An adjacency matrix requires an n2 element
+                    array so for parse graphs much of the matrix
+                    will be empty. Also, for undirected graphs half
+                    of the graph is repeated information. Hence
+                    these matrices are said to be space
+                    inefficient.
+                </p>
+                <p className="my-6">
+                    The memory use of an adjacency matrix is
+                    O(n2 ) where n = number of vertices.
+                </p>
+                <p>
+                    Advantages of using adjacency matrix are as follows:
+                    <ul className="list-disc list-inside">
+                        <li>
+                            Easy to understand, implement and
+                            convenient to work with.
+                        </li>
+                        <li>
+                            Removing an edge involves time
+                            complexity of O(1).
+                        </li>
+                        <li>
+                            Queries like whether there is an edge
+                            from vertex „u‟ to vertex „v‟ are
+                            efficient and require time complexity,
+                            O(1).
+                        </li>
+                    </ul>
+                </p>
+                <p>
+                    Disadvantages of using adjacency matrix are as follows:
+                    <ul className="list-disc list-inside">
+                        <li>
+                            Space complexity is of the O(V2 )
+                            where V = number of vertices.
+                        </li>
+                        <li>
+                            Sparse matrix has fewer edges but the
+                            space complexity is still the same.
+                        </li>
+                        <li>
+                            Adding an edge involves time
+                            complexity of O(V2 ).
+                        </li>
+                        <li>
+                            If the number of nodes in the graph
+                            may change, matrix representation is
+                            too inflexible (especially if we don't
+                            know the maximal size of the graph).
+                        </li>
+                    </ul>
+                    This is preferred if the graph is of dense type
+                    where |E| ~ V 2 .
+                </p>
+                <div className="flex justify-center my-6">Fig. 6. Adjacency Matrix Representation of a given Graph</div>
+                <div className="flex justify-center my-6 text-2xl font-semibold">Code(Adjacency Matrix)</div>
             </div>
+            <p>
+                Although the linked list representation
+                requires very less memory as compared to
+                the adjacency matrix, the simplicity of the
+                adjacency matrix makes it preferable when
+                graphs are reasonably small.
+            </p>
+            <p>
+                Adjacency information in an array can be
+                viewed as a function.
+                <ol  className="list-decimal list-inside">
+                    <li>
+                        Merits of representing graphs as functions
+                        are as follows:
+                        <ul  className="list-disc list-inside">
+                            <li>
+                                Simple and easy to understand.
+                            </li>
+                            <li>
+                                Easily adaptable to different types of
+                                graphs.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>    
+                        Demerits of representing graphs as
+                        functions are as follows:
+                        <ul  className="list-disc list-inside">
+                            <li>
+                                Graph must be known statistically at
+                                compile time.
+                            </li>
+                            <li>
+                                Cannot be extended to accommodate
+                                queries about the set of Vertices or the
+                                set of Edges.
+                            </li>
+                        </ul>
+                    </li>
+                </ol>
+            </p>
+            <p>
+                One way to overcome the cons of using
+                functions to represent graphs is to use arrays
+                instead.
+                <ol className="list-decimal list-inside">
+                    <li>
+                        Merits of representing graphs as arrays are
+                        as follows:
+                        <ul className="list-disc list-inside">
+                            Simple and easy to understand and
+                            easily adaptable to different types of
+                            graphs.
+                        </ul>
+                        <ul>
+                            Can be accessed efficiently and
+                            constructed at runtime.
+                        </ul>
+                    </li>
+                    <li>
+                        Demerits of representing graphs as arrays
+                        are as follows:
+                        <ul className="list-disc list-inside">
+                            <li>
+                                The domain of Vertices must be a type
+                                that can be used as an index into an
+                                array.
+                            </li>
+                            <li>
+                                Requires that graph access be a
+                                Command rather than a computation
+                            </li>
+                        </ul>
+                    </li>
+                </ol>
+            </p>
         </div>
     )
 }
