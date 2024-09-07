0b854c6a5a5eb2e743bb40b5aff64fd37241d6b1
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2024-09-07 07:00:19 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -17,30 +17,22 @@ function App() {
 
   return (
     <div className="bg-gradient-to-b from-[#1a1a2e] via-[#16213e] to-[#0f3460] text-[#e5e5e5] min-h-screen">
-      {/* Header Section */}
       <div className="reveal">
         <Header />
       </div>
 
-      {/* Main Content Grid */}
       <div className="grid grid-cols-12 w-full">
-        {/* Sidebar */}
         <div className="col-span-2 sticky top-0 reveal">
           <Sidebar />
         </div>
 
-        {/* Content Section */}
         <div className="col-span-8 p-8 reveal">
           <Content />
         </div>
-
-        {/* Extra Space for Sidebar alignment */}
         <div className="col-span-2"></div>
       </div>
-
-      {/* Footer Section */}
       <div className="w-full flex justify-center py-4 text-white bg-[#1c1c1c] reveal mt-8">
-        <span>&#x00A9; Made with ❤️ by Jagjeevan Kashid</span>
+        <span>&#x00A9; Made with ❤️ by <a href="https://www.linkedin.com/in/jagjeevan-kashid-2b7999192/">Jagjeevan Kashid</a></span>
       </div>
     </div>
   );
